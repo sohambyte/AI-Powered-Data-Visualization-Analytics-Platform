7e73aee90435a782f1500cf6fb35be23226f1c0c
Soham Wankhede <161970786+sohamwankhede2022@users.noreply.github.com>
2025-02-12 11:38:16 +0530

    .

---
+++ Frontend/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <h2>Infotact Solution</h2>
+      <h1>Hello <World></World></h1>
+    </div>
+  );
+};
+
+export default App;
